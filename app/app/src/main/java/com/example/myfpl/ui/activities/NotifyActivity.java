62f70fa4a01e93b0f4f495a7601a223740367d97
raikojima <duyvo761@gmail.com>
2023-07-20 22:45:17 +0700

    feat: notification
    Notification screen and detail

--- app/app/src/main/java/com/example/myfpl/ui/activities/NotifyActivity.java
+++ app/app/src/main/java/com/example/myfpl/ui/activities/NotifyActivity.java
@@ -1,18 +1,62 @@
 package com.example.myfpl.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.myfpl.adapters.NotificationFragmentAdapter;
 import com.example.myfpl.databinding.ActivityNotifyBinding;
+import com.example.myfpl.models.NotificationModel;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
 
 public class NotifyActivity extends AppCompatActivity {
     private ActivityNotifyBinding binding;
+    private NotificationFragmentAdapter adapter;
+    private ArrayList<NotificationModel> list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityNotifyBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        list = new ArrayList<>();
+        init();
+    }
+    public void init(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new NotificationFragmentAdapter(fragmentManager, getLifecycle());
+        binding.viewPager.setAdapter(adapter);
+        new TabLayoutMediator(binding.topTab, binding.viewPager, (tab, position) -> {
+            switch (position){
+                case 0:
+                    tab.setText("Tất cả");
+                    break;
+                case 1:
+                    tab.setText("Học tập");
+                    break;
+                case 2:
+                    tab.setText("Học phí");
+                    break;
+                case 3:
+                    tab.setText("Hoạt động");
+                    break;
+                case 4:
+                    tab.setText("Việc làm");
+                    break;
+            }
+        }).attach();
 
+
+    }
+        public void goToDetail(NotificationModel object, String date){
+        Intent i = new Intent(NotifyActivity.this, DetailNotificationActivity.class);
+        i.putExtra("detail",object);
+        i.putExtra("createdAt",date);
+        this.startActivity(i);
     }
 }
---
+++ app/app/src/main/java/com/example/myfpl/ui/fragments/NotificationFragment.java
@@ -0,0 +1,150 @@
+package com.example.myfpl.ui.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myfpl.R;
+import com.example.myfpl.adapters.NotificationAdapter;
+import com.example.myfpl.databinding.FragmentNotificationBinding;
+import com.example.myfpl.models.NotificationModel;
+import com.example.myfpl.ui.activities.DetailNotificationActivity;
+import com.example.myfpl.util.ToastApp;
+
+import java.util.ArrayList;
+
+
+public class NotificationFragment extends Fragment {
+    private FragmentNotificationBinding binding;
+
+    private String type;
+    private ArrayList<NotificationModel> list;
+    public NotificationFragment() {
+
+    }
+    public static NotificationFragment instance(String type){
+        NotificationFragment fragment = new NotificationFragment();
+        Bundle args = new Bundle();
+        args.putString("type", type);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+          type = getArguments().getString("type");
+        }
+        addData();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentNotificationBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+       NotificationAdapter adapter = new NotificationAdapter(filteredList(type, list), getContext());
+        binding.rvNotification.setAdapter(adapter);
+
+    }
+    public ArrayList<NotificationModel> filteredList(String type, ArrayList<NotificationModel> unfilteredList){
+        if(type=="Tất cả") return unfilteredList;
+        ArrayList<NotificationModel> filtered = new ArrayList<>();
+        for (NotificationModel element: unfilteredList ) {
+            if(element.getType()==type){
+                filtered.add(element);
+            }
+        }
+        return filtered;
+    }
+
+    public void addData(){
+        list = new ArrayList<>();
+        list.add(new NotificationModel(
+                "nt01",
+                "Thông báo hộ trợ sinh viên đăng ký BHYT tháng 6/2023",
+                "nhitq",
+                "Học phí",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+                "2023-07-13 00:39:30",
+                "2023-07-13 00:39:30"
+        ));
+        list.add(new NotificationModel(
+                "nt02",
+                "Thông báo hộ trợ sinh viên đăng ký BHYT tháng 6/2023",
+                "nhitq",
+                "Học phí",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+                "2023-07-13 00:39:30",
+                "2023-07-13 00:39:30"
+        ));
+        list.add(new NotificationModel(
+                "nt02",
+                "Thông báo hộ trợ sinh viên đăng ký BHYT tháng 6/2023",
+                "nhitq",
+                "Hoạt động",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+                "2023-07-13 00:39:30",
+                "2023-07-13 00:39:30"
+        ));
+        list.add(new NotificationModel(
+                "nt04",
+                "Thông báo hộ trợ sinh viên đăng ký BHYT tháng 6/2023",
+                "nhitq",
+                "Hoạt động",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+                "2023-07-13 00:39:30",
+                "2023-07-13 00:39:30"
+        ));
+        list.add(new NotificationModel(
+                "nt05",
+                "Thông báo hộ trợ sinh viên đăng ký BHYT tháng 6/2023",
+                "nhitq",
+                "Học tập",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+                "2023-07-13 00:39:30",
+                "2023-07-13 00:39:30"
+        ));
+        list.add(new NotificationModel(
+                "nt08",
+                "Thông báo hộ trợ sinh viên đăng ký BHYT tháng 6/2023",
+                "nhitq",
+                "Học tập",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+                "2023-07-13 00:39:30",
+                "2023-07-13 00:39:30"
+        ));
+        list.add(new NotificationModel(
+                "nt08",
+                "Thông báo hộ trợ sinh viên đăng ký BHYT tháng 6/2023",
+                "nhitq",
+                "Việc làm",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+                "2023-07-13 00:39:30",
+                "2023-07-13 00:39:30"
+        ));     list.add(new NotificationModel(
+                "nt08",
+                "Thông báo hộ trợ sinh viên đăng ký BHYT tháng 6/2023",
+                "nhitq",
+                "Việc làm",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+                "2023-07-13 00:39:30",
+                "2023-07-13 00:39:30"
+        ));
+    }
+
+}
