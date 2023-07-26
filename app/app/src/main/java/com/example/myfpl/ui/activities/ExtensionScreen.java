16d03ee3a622151a3500e4d806293d4bf0d25e11
tu276fnnn <qtuvip@gmail.com>
2023-07-26 17:12:22 +0700

    feat: extensionScreen

---
+++ app/app/src/main/java/com/example/myfpl/ui/activities/ExtensionScreen.java
@@ -0,0 +1,32 @@
+package com.example.myfpl.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.myfpl.R;
+import com.example.myfpl.databinding.ActivityExtensionScreenBinding;
+
+public class ExtensionScreen extends AppCompatActivity {
+
+    private ActivityExtensionScreenBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extension_screen);
+        binding = ActivityExtensionScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        init();
+    }
+    public void init(){
+
+    }
+    public void onBack(View view){
+        Intent intent = new Intent(ExtensionScreen.this, SplashScreen.class);
+        startActivity(intent);
+        finish();
+    }
+}
